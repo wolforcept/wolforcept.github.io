f5dc6595a64e7fbc016985f6e2e37add5c626687
wolforce <miguelfeotorres@gmail.com>
2023-10-02 14:54:55 +0100

    worddish mirror

--- webgames.js
+++ webgames.js
@@ -1,14 +1,15 @@
 function createElement() {
     return html`
         <h1>Web Games</h1>
-        <my-card title='PokeIdler'  url='./pokeidler' 			pixelated='true'	image='./images/pokeball.png'></my-card>
-        <my-card title='Concept'    url='./concept/' 	                            image='./images/concept2.png'></my-card>
-        <my-card title='LoL Blurs'  url='./blurs/lol' 								image='./images/lolblurs128.png'></my-card>
+        <my-card title='PokeIdler'  url='./pokeidler' 			pixelated='true'	image='./images/webgames/pokeball.png'></my-card>
+        <my-card title='Concept'    url='./concept/' 	                            image='./images/webgames/concept2.png'></my-card>
+        <my-card title='LoL Blurs'  url='./blurs/lol' 								image='./images/webgames/lolblurs128.png'></my-card>
         <my-card title='LoL Quiz'   url='./lolquiz' 								image='./images/webgames/lolquiz.png'></my-card>
         <my-card title='Poke Blurs' url='./blurs/pokemon' 							image='./images/webgames/pokeblurs.png'></my-card>
-        <my-card title='Cobra 2'    url='./Cobra2' 				pixelated='true'	image='./images/cobra2.png'></my-card>
+        <my-card title='WordDish'   url='./worddish'			                	image='./images/webgames/worddish.png'></my-card>
+        <my-card title='Cobra 2'    url='./Cobra2' 				pixelated='true'	image='./images/webgames/cobra2.png'></my-card>
         <my-card title='Mover'      url='./mover' 									image='./mover/icon.png'></my-card>
-        <my-card title='AutoEP'     url='./AutoEP' 									image='./images/autoep400.png'></my-card>
+        <my-card title='AutoEP'     url='./AutoEP' 									image='./images/webgames/autoep400.png'></my-card>
         <h1>Mobile Games</h1>
         <my-card title='Wordaria'   url='https://play.google.com/store/apps/details?id=wolforce.games.wordaria' image='./images/mobile/wordaria.png'></my-card>
     `
